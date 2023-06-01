2ee7b47fb10b227489cbd4a49f82171d41fd3b13
LucasMartinez24 <martinezlucas1478@gmail.com>
2023-06-01 18:37:10 -0300

    aux

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -27,6 +27,7 @@ export class NavComponent implements OnInit {
   }
   logOut() {
     this.auth.logoutUser();
+    this.router.navigateByUrl('/inicio')
   }
   //NAVBAR
   @HostListener('window:scroll', [])
